77fbbe56f581c07b43c7d4d540d7ca3db707bf5b
Illya Vlasenko <illyavlasenko2004@gmail.com>
2022-10-02 17:55:11 +0300

    initial commit

---
+++ Rectangle/Rectangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter side1=");
+            double side1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter side2=");
+            double side2 = double.Parse(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(side1,side2);
+
+            rectangle.Print();
+           
+        }
+    }
+}
---
+++ Rectangle/Rectangle/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+internal class Rectangle
+{
+    private double side1, side2 ;
+    public double Side1 { get { return side1; } set { side1 = value; } }
+    public double Side2 { get { return side2; } set { side2 = value; } }
+    public double Area { get { return AreaCalculator(); } }
+    public double Perimetr { get { return PerimeterCalculator(); } }
+    public Rectangle (double side1, double side2 )
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+    double AreaCalculator()
+    {
+        return side1 * side2;
+    }
+    double PerimeterCalculator()
+    {
+        return (side1 + side2) * 2;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"\nArea={AreaCalculator()} \nPerimetr={PerimeterCalculator()}");
+        Console.ReadLine();
+    }
+
+}
