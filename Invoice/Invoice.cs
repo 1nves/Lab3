f96f3c3ebac9f81ed8a7414537ed51dd4fec3f52
Illya Vlasenko <illyavlasenko2004@gmail.com>
2022-10-07 15:22:05 +0300

    Add Invoice

---
+++ Invoice/Invoice.cs
@@ -0,0 +1,45 @@
+﻿using System;
+public class Invoice
+{
+    private readonly int account;
+    private readonly string customer;
+    private readonly string provider;
+
+    private string article;
+    private int quantity;
+
+    private double percantage = 20.0;
+
+    public Invoice(int account, string customer, string provider, string article, int quantity)
+    {
+        this.account = account;
+        this.customer = customer;
+        this.provider = provider;
+        this.article = article;
+        this.quantity = quantity; 
+    }
+
+    public double PriceWithoutVAT()
+    {
+        double sum = (double)account * quantity;
+        return sum;
+    }
+    public double VAT()
+    {
+        double vat = (double)account * quantity * (percantage/100);
+        return vat;
+    }
+
+    public double PriceWithVAT()
+    {
+        VAT();
+        double sum = VAT() + (double)account * quantity;
+        return sum;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Price without VAT = {PriceWithoutVAT()}");
+        Console.WriteLine($"Price with VAT = {PriceWithVAT()}");
+    }
+}
---
+++ Invoice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice(400," Illya","McDonalds","Article about fastfood", 45);
+
+            invoice.Print();
+            Console.ReadLine();
+        }
+    }
+}
